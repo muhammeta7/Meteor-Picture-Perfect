f36135739f10a6266ad1b2367deb2ad66b50a740
Muhammet <muhammeta7@gmail.com>
2016-12-14 20:25:24 -0500

    created error message for page not found

--- server/main.js
+++ server/main.js
@@ -1,5 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
+  BrowserPolicy.content.allowOriginForAll('*');
   // code to run on server at startup
+
+  // create admin from settings
+  if (Meteor.users.findOne(Meteor.settings.adminId)){
+    Roles.addUsersToRoles(Meteor.settings.adminId, ['admin']);
+  }
 });
